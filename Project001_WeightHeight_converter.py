ddc9fd0889c2bc1ab31d86645daf939bd3a32eb3
c-annabel <106787550+c-annabel@users.noreply.github.com>
2022-10-07 03:14:37 +0200

    Weight and Height converter

---
+++ Project001_WeightHeight_converter.py
@@ -0,0 +1,56 @@
+#==========Weight & Height converter===========#
+def weight_n_height_converter():
+    global conv_type_input
+    conv_type_input  = input(
+        '''Please choose a valid converter from the list below: 
+        1 (kg'->'oz)  / 2 (oz'->'kg)
+        3 (kg'->'lbs) / 4 (lbs'->'kg)
+        5 (cm'->'in)  / 6 (in'->'cm)
+        7 (cm'->'ft)  / 8 (ft'->'cm)
+        : ''')
+
+conv_types = {"1":"kg", "2":"oz", "3":"kg", "4":"lbs", 
+            "5":"cm", "6":"in", "7":"cm", "8":"ft"}
+
+conv_type_input = ""
+while conv_type_input not in conv_types:
+    weight_n_height_converter()
+
+weightHeight = float(input(f"Please enter your height/weight in {conv_types[conv_type_input]}: "))
+
+weightHeight_before = weightHeight
+conv_type_input_before = conv_type_input
+conv_kgoz_rate = 35.27396195
+conv_kglb_rate = 2.20462262185
+conv_cmin_rate = 2.54
+conv_cmft_rate = 30.48
+
+if conv_type_input == "1":
+    weightHeight = "{:.2f}".format(weightHeight * conv_kgoz_rate)
+    conv_type_input = "2"
+elif conv_type_input == "2":
+    weightHeight = "{:.2f}".format(weightHeight / conv_kgoz_rate)
+    conv_type_input = "1"
+elif conv_type_input == "3":
+    weightHeight = "{:.2f}".format(weightHeight * conv_kglb_rate)
+    conv_type_input = str(int(conv_type_input)+1)
+elif conv_type_input == "4":
+    weightHeight = "{:.2f}".format(weightHeight / conv_kglb_rate)
+    conv_type_input = str(int(conv_type_input)-1)
+elif conv_type_input == "5":
+    weightHeight = "{:.2f}".format(weightHeight / conv_cmin_rate)
+    conv_type_input = "6"
+elif conv_type_input == "6":
+    weightHeight = "{:.2f}".format(weightHeight * conv_cmin_rate)
+    conv_type_input = "5"
+elif conv_type_input == "7":
+    weightHeight = "{:.2f}".format(weightHeight / conv_cmft_rate)
+    conv_type_input = "8"
+elif conv_type_input == "8":
+    weightHeight = "{:.2f}".format(weightHeight * conv_cmft_rate)
+    conv_type_input = "7"
+else:
+    weightHeight = 0
+
+print(f"Result: {weightHeight_before} {conv_types[conv_type_input_before]} = {weightHeight} {conv_types[conv_type_input]}. ")
+
